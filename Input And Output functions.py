113d706c8087b1218a26ddbe11bab3e1b7acf983
SyedMahdi1 <131530472+SyedMahdi1@users.noreply.github.com>
2025-12-24 14:12:37 +0500

    Add files via upload

---
+++ Input And Output functions.py
@@ -0,0 +1,19 @@
+print(int(input("Input Any Integer Number")))
+print(float(input("Input ANy Number HAving Decimal point")))
+print(str(input("Input any name")))
+
+a=int(input("Enter Any Number:\t"))
+print("U entered:\t",a)
+
+a=input("Enter UR name")
+print("UR Name IS:",a)
+
+# To find the square of two input numbers
+t=int(input("Enter The First Number"))
+y=int(input("Enter The Second Number"))
+print("Square of two given numbers will be",t**y)
+
+# or
+x=input("Enter The First Number")
+c=input("Enter The Second Number")
+print("Square of two given numbers will be",int(x)**int(c))
